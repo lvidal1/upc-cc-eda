643017b0bea69ddb511fe8862aca13d699744ab0
leonardo <leonardo@caffelli.com>
2022-05-16 23:05:31 -0500

    Add casting examples

--- src/Clase1.java
+++ src/Clase1.java
@@ -20,6 +20,28 @@ public class Clase1 {
         final double NE = 2.82;
         System.out.print("PI equivale a: "+PI + "\n");
         System.out.println("NE = "+ NE);
+
+
+        // Casting examples
+        System.out.println("Conversiones");
+        String word = "1234";
+        int number = Integer.parseInt(word);
+        System.out.println(number);
+
+        String word2 = "12.4";
+        double decimal = Double.parseDouble(word2);
+        System.out.println(decimal);
+
+        String word3 = "12.5";
+        float decimal2 = Float.parseFloat(word3);
+        System.out.println(decimal2);
+
+        int integer = 123;
+        String castedWord = String.valueOf(integer);
+        System.out.println(castedWord);
+
+        String chainedWord = Integer.toString(integer);
+        System.out.println(chainedWord);
     }
 
 }
