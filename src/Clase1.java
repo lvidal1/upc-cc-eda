88915c44d7a44c8b3eaf73c77883c9368a34e17c
leonardo <leonardo@caffelli.com>
2022-05-16 22:41:18 -0500

    Add Clase1 file

---
+++ src/Clase1.java
@@ -0,0 +1,25 @@
+public class Clase1 {
+
+    public static void main (String[] args) {
+
+        String alias = "Michael";
+        int age = 30;
+        int otherAge = 21;
+        int negative = -1;
+        String name = "Leonardo";
+
+        System.out.println("Mi nombre es "+ name + " y tengo " + age);
+
+        char char1, char2;
+        char1 = 'a';
+        char2 = 'b';
+
+        System.out.println("Concatenación de letra es: "+ char1 + " y "+ char2);
+
+        final double PI = 3.1416;
+        final double NE = 2.82;
+        System.out.print("PI equivale a: "+PI + "\n");
+        System.out.println("NE = "+ NE);
+    }
+
+}
